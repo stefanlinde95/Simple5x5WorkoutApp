561345beeb65f17d30fb2aec24e2adc57b874732
stefanl <stefanlinde1995@gmail.com>
2022-03-29 17:44:50 +0300

    port

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 3001;
-
 app.use(cors());
 app.use(express.json());
 
@@ -33,6 +31,6 @@ const usersRouter = require("./routes/users");
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port: ${port}`);
 });
